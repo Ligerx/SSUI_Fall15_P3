a260d10cac5b5bb3e5c75df367030109d0ae550d
awang1 <awang1@andrew.cmu.edu>
2015-10-25 23:56:44 -0400

    WOrking on character base, not complete yet

--- app/src/main/java/ssuimobile/gameengine/GameCharacterBase.java
+++ app/src/main/java/ssuimobile/gameengine/GameCharacterBase.java
@@ -1,24 +1,53 @@
 package ssuimobile.gameengine;
 
-import ssuimobile.gameengine.event.FSMEvent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+
+import ssuimobile.gameengine.action.FSMAction;
+import ssuimobile.gameengine.event.FSMEvent;
 
 public class GameCharacterBase extends GameCharacterPreBase {
 
 	@Override
 	public boolean deliverEvent(FSMEvent event) {
+		// Guard if there are no states
+		if(_FSMStateTable == null || _FSMStateTable.length == 0) return false;
+
+		// Loop through all the states of the FSM
+//		for(int i = 0; i < _FSMStateTable.length; i++) {
+//			FSMState state = _FSMStateTable[i];
+		for(FSMState state : _FSMStateTable) {
+			// Look for a transition that takes the event
+			for(int j = 0; j < state._transitions.length; j++) {
+				FSMTransition transition = state.getTransitionAt(j);
+				if(transition.match(event)) {
+					// Call the transition and return true to signify consuming the event
+					makeFSMTransition(transition, event);
+					return true;
+				}
+			}
+		}
+
 		return false;
 	}
 
 	@Override
 	protected void makeFSMTransition(FSMTransition transition, FSMEvent evt) {
-		
+		FSMAction[] actions = transition.getAction();
+		int targetState = transition.getTargetState();
+
+		for(FSMAction action : actions) {
+			action.
+		}
+
+		// Update the state after running all transition actions
+		_currentState = targetState;
 	}
 
 	@Override
 	public void draw(Canvas canv) {
-		
+		// by default, getImage has a null value, so this should be safe
+		canv.drawBitmap(getImage(), getX(), getY(), null);
 	}
 
 
@@ -30,7 +59,7 @@ public class GameCharacterBase extends GameCharacterPreBase {
 			FSMState[] states, 
 			Bitmap img) 
 	{
-
+		super(owner, index, x, y, w, h, states, img);
 	}
 
 }
