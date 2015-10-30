31562bcfc87926d53769ece714dc1910c656d87a
awang1 <awang1@andrew.cmu.edu>
2015-10-30 18:28:45 -0400

    Change buttonHit to dispatchTryAll. I don't know what is right, but this works based on the xml

--- app/src/main/java/ssuimobile/gameengine/GameEngineBase.java
+++ app/src/main/java/ssuimobile/gameengine/GameEngineBase.java
@@ -187,16 +187,8 @@ public class GameEngineBase extends GameEnginePreBase {
 
 		ButtonPressedEvent event = new ButtonPressedEvent(buttonNum);
 
-		// loop through all characters
-		// get current state object
-		// check if the state has any matching transition/event matches
-
-//		for(GameCharacter character : _characters) {
-//			Log.d("buttonHit", "iterating through characters");
-//			if(character.deliverEvent(event)) break;
-//		}
-
-		dispatchToAll(event);
+//		dispatchToAll(event);
+		dispatchTryAll(event);
 	}
 	
 	@Override
